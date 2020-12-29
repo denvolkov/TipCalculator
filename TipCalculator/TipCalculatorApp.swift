aef3800514f88cbf1fcbc9e983a4fbd9dc3bde75
denvolkov <denvolkov@inbox.ru>
2020-12-29 13:52:09 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by test on 29.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
