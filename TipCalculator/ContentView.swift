4884fa73b7a6d6e98ba6dc609d9792abdd49b962
denvolkov <denvolkov@inbox.ru>
2020-12-29 14:21:15 +0300

    Finish

--- TipCalculator/ContentView.swift
+++ TipCalculator/ContentView.swift
@@ -2,15 +2,56 @@
 //  ContentView.swift
 //  TipCalculator
 //
-//  Created by test on 29.12.2020.
+//  Created by Denis Volkov on 29.12.2020.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercent = 0
+    
+    let tipPercents = [0, 5, 10, 15]
+    
+    var totalPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercents[tipPercent])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amounPerson = grandTotal / peopleCount
+        
+        return amounPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount, ₽", text: $checkAmount)
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<10) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                Section(header: Text("Сколько чаевых вы хотите оставить?")) {
+                    Picker("Tippercentage", selection: $tipPercent) {
+                        ForEach(0..<tipPercents.count) {
+                            Text("\(self.tipPercents[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("\(totalPerson, specifier: "%.2f")₽")
+                }
+            }
+            .navigationBarTitle("Калькулятор чаевых", displayMode: .inline)
+        }
     }
 }
 
